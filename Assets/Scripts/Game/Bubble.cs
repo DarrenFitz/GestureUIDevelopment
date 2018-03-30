655e8028d1bb420e07168f7de395c0313361260d
Darren Fitzpatrick <dfitzpatrick12@gmail.com>
2018-03-30 01:06:06 +0100

    hands collision object

---
+++ Assets/Scripts/Game/Bubble.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+using System.Collections.Generic;
+
+using Windows.Kinect;
+using Joint = Windows.Kinect.Joint;
+
+public class Bubble : MonoBehaviour
+{
+    public Vector3 mMovementDirection = Vector3.zero;
+    public Coroutine mCurrentChanger = null;
+
+    private void OnEnable()
+    {
+        mCurrentChanger = StartCoroutine(DirectionChanger());
+    }
+
+    private void OnDisable()
+    {
+        StopCoroutine(mCurrentChanger);
+    }
+
+    private void OnBecameInvisible()
+    {
+        gameObject.SetActive(false);
+        // Return to BubbleManager
+    }
+
+    private void Update()
+    {
+        // Movement
+        transform.position += mMovementDirection * Time.deltaTime * 0.5f;
+    }
+
+    private IEnumerator DirectionChanger()
+    {
+        while (gameObject.activeSelf)
+        {
+            mMovementDirection = new Vector2(Random.Range(0,100) * 0.01f, Random.Range(0,100) * 0.01f);
+
+            yield return new WaitForSeconds(3.0f);
+        }
+    }
+}
---
+++ Assets/Scripts/Game/Hand.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine.Networking;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Windows.Kinect;
+using Joint = Windows.Kinect.Joint;
+
+public class Hand : MonoBehaviour {
+
+    public Transform mHandMesh;
+
+	private void Update () {
+        mHandMesh.position = Vector3.Lerp(mHandMesh.position, transform.position, Time.deltaTime * 15.0f);
+	}
+	
+	private void OnTriggerEnter2D (Collider2D collision) {
+
+        if (!collision.gameObject.CompareTag("Bubble"))
+            return;
+
+        collision.gameObject.SetActive(false);
+	}
+}
